6ab8379f03c3826fd46f2e89d1cf6f73d376bf13
slthurman01 <slthurman01@gmail.com>
2023-01-12 12:48:53 -0600

    Adds inquirer prompts with references to functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,23 +19,51 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-const prompt = [ 
-  {
-      name: "choice",
+inquirer
+  .prompt([
+    {
+      name: "trackerSelect",
       type: "list",
       message: "What would you like to do?",
-      choices: [ 
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an mployee",
-          "Update an employee role",
-          "Quit"
-      ]  
-  }
-];
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Quit"
+      ]
+    }
+  ])
+  .then((answer) => {
+    if (answer.trackerSelect == "View all departments") {
+      viewDepts();
+    } else if (answer.trackerSelect == "View all roles") {
+      viewRoles();
+    } else if (answer.trackerSelect == "View all employees") {
+      viewEmployees();
+    } else if (answer.trackerSelect == "Add a department") {
+      addDept();
+    } else if (answer.trackerSelect == "Add a role") {
+      addRole();
+    } else if (answer.trackerSelect == "Add an employee") {
+      addEmployee();
+    } else if (answer.trackerSelect == "Update an employee role") {
+      updateRole();
+    } else if (answer.trackerSelect == "Quit") {
+      console.log("Thanks for using our employee tracker!")
+      connection.end();
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 // view all departments
 function viewDepts() {
@@ -49,35 +78,35 @@ function viewRoles() {
 };
 
 // view all employees
-function viewEmployees(){
+function viewEmployees() {
 
 
 };
 
 // add a department
-function addDept(){
-app.post('/api/new-department', ({ body }, res) => {
-  const sql = `INSERT INTO department (dept_name)
+function addDept() {
+  app.post('/api/new-department', ({ body }, res) => {
+    const sql = `INSERT INTO department (dept_name)
     VALUES (?)`;
-  const params = [body.dept_name];
+    const params = [body.dept_name];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
     });
   });
-});
 };
 
 // add a role
 function addRole() {
-app.post('/api/new-role', ({ body }, res) => {
-  const sql = `INSERT INTO role (
+  app.post('/api/new-role', ({ body }, res) => {
+    const sql = `INSERT INTO role (
     title,
     salary,
     dept_id
@@ -86,25 +115,25 @@ app.post('/api/new-role', ({ body }, res) => {
       ?,
       ?,
       ?)`;
-  const params = [body.title, body.salary, body.dept_id];
+    const params = [body.title, body.salary, body.dept_id];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
     });
   });
-});
 };
 
 // add an employee
-function addEmployee(){
-app.post('/api/new-employee', ({ body }, res) => {
-  const sql = `INSERT INTO employee (
+function addEmployee() {
+  app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (
     first_name,
     last_name,
     role_id,
@@ -115,23 +144,23 @@ app.post('/api/new-employee', ({ body }, res) => {
       ?,
       ?,
       ?)`;
-  const params = [body.first_name, body.las_name, body.role_id, body.manager_id];
+    const params = [body.first_name, body.las_name, body.role_id, body.manager_id];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
     });
   });
-});
 };
 
 // update an employee role
-function updateRole(){
+function updateRole() {
 
 
 };
